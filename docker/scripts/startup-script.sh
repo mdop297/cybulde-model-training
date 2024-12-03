d3d2eb86204f2575ec527606a675155534bd390a
mdop297 <lenhatminh297@gmail.com>
2024-12-04 00:40:37 +0700

    setup docker container and tracking server

--- docker/scripts/startup-script.sh
+++ docker/scripts/startup-script.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-tail -F anythoig
+set -o errexit
+set -o pipefail
+set -o nounset
+
+if [[ "${IS_PROD_ENV}" == "true" ]]; then
+	# /usr/local/gcloud/google-cloud-sdk/bin/gcloud compute ssh "${VM_NAME}" --zone "${ZONE}" --tunnel-through-iap -- -4 -N -L ${PROD_MLFLOW_SERVER_PORT}:localhost:${PROD_MLFLOW_SERVER_PORT}
+    echo "production service"
+else
+	# /start-prediction-service.sh &
+	/start-tracking-server.sh &
+	tail -F anything
+fi
